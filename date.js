91759e25b0aadaa310ace664b84bbae7d4a049b4
Yi-Hao Chiu <chokeslam46@gmail.com>
2019-06-01 14:48:10 +0800

    add fido2 features

---
+++ date.js
@@ -0,0 +1,67 @@
+const padding = (val, len = 2) => {
+  let padded = '';
+  for (let index = 0; index < len; index++) {
+      padded += '0';
+  }
+  const result = `${padded}${val}`
+  return result.slice(result.length - len);
+}
+
+const dateToDays = () => {
+  const date = new Date($('#mfc-date').val());
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const dayOfMonth = date.getDate();
+
+  const days = ((year - 1980) << 9) + (month << 5) + dayOfMonth;
+
+  $('#mfc-days').val(padding(days.toString(16).toUpperCase(), 4));
+  showResult();
+}
+
+const daysToDate = () => {
+  let days;
+  try {
+      days = parseInt($('#mfc-days').val(), 16);
+      $('#mfc-days').val(padding(days.toString(16).toUpperCase(), 4));
+  } catch (error) {
+      console.error(error);
+      init();
+      return;
+  }
+
+  const year = (days >> 9) + 1980;
+  const month = (days & 0x1F0) >> 5;
+  const dayOfMonth = days & 0x1F;
+
+  if (year === 0 || month === 0 || dayOfMonth === 0) {
+      console.error(`invalid date: ${year}-${padding(month)}-${padding(dayOfMonth)}`);
+      init();
+      return;
+  }
+
+  $('#mfc-date').val(`${year}-${padding(month)}-${padding(dayOfMonth)}`);
+
+  showResult();
+}
+
+const showResult = () => {
+  let days;
+  let daysByteString;
+  try {
+      days = parseInt($('#mfc-days').val(), 16);
+      daysByteString=`${padding((days & 0xFF).toString(16).toUpperCase())} ${padding((days >> 8).toString(16).toUpperCase())}`;
+  } catch (error) {
+      console.error(error);
+      init();
+      return;
+  }
+  
+  $('#result').html(`
+      <div class="col-2">S7B0</div>
+      <div class="col-10">4A ${daysByteString} 00 00 00 00 00 00 00 00 00 1E 00 00 00 00 00</div>
+      <div class="col-2">S8B0</div>
+      <div class="col-10">4A ${daysByteString} 00 00 00 00 00 1E 00 00 00 00 00 00 00 00 00</div>
+  `);
+}
+
---
+++ fido2.js
@@ -0,0 +1,206 @@
+const register = () => {
+  const email = $('#email').val();
+  const displayname = $('#displayname').val();
+  if (email === '' || displayname === '') {
+    throw Error('empty data');
+  }
+
+  fetch('https://r02921049.ddns.net:10443/v0/register/begin', {
+    method: 'POST',
+    credentials: 'include',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    mode: 'cors',
+    body: JSON.stringify({ email, displayname }),
+  })
+  .then((response) => {
+    if (response.status !== 200) {
+      throw Error(response.statusText);
+    }
+    return response.json();
+  })
+  .then((response) => {
+    createCredential(response);
+  })
+  .catch(console.error);
+};
+
+const createCredential = (data) => {
+  data.publicKey.challenge = base64url.decode(
+    base64url.fromBase64(data.publicKey.challenge)
+  );
+  data.publicKey.user.id = base64url.decode(
+    base64url.fromBase64(data.publicKey.user.id)
+  );
+
+  navigator.credentials.create(data).then((response) => {
+    const data = publicKeyCredentialToJSON(response);
+    sendRegisterResponse(data);
+  });
+};
+
+const publicKeyCredentialToJSON = (pubKeyCred) => {
+  if (pubKeyCred instanceof Array) {
+    let arr = [];
+    for (let i of pubKeyCred) arr.push(publicKeyCredentialToJSON(i));
+
+    return arr;
+  }
+
+  if (pubKeyCred instanceof ArrayBuffer) {
+    return base64url.encode(pubKeyCred);
+  }
+
+  if (pubKeyCred instanceof Object) {
+    let obj = {};
+
+    for (let key in pubKeyCred) {
+      obj[key] = publicKeyCredentialToJSON(pubKeyCred[key]);
+    }
+
+    return obj;
+  }
+
+  return pubKeyCred || "";
+};
+
+const sendRegisterResponse = (data) => {
+  fetch('https://r02921049.ddns.net:10443/v0/register/finish', {
+    method: 'POST',
+    credentials: 'include',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    mode: 'cors',
+    body: JSON.stringify(data),
+  })
+  .then((response) => {
+    if (response.status !== 200) {
+      throw Error(response.statusText);
+    }
+    return response.json();
+  })
+  .then((response) => {
+    console.log(response);
+  })
+  .catch(console.error);
+};
+
+const login = () => {
+  const email = $('#login_email').val();
+  if (!email) {
+    throw Error('empty data');
+  }
+
+  fetch('https://r02921049.ddns.net:10443/v0/login/begin', {
+    method: 'POST',
+    credentials: 'include',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    mode: 'cors',
+    body: JSON.stringify({ email }),
+  })
+  .then((response) => {
+    if (response.status !== 200) {
+      throw Error(response.statusText);
+    }
+    return response.json();
+  })
+  .then((response) => {
+    assertCredential(response);
+  })
+  .catch(console.error);
+};
+
+const assertCredential = (data) => {
+  data.publicKey.challenge = base64url.decode(
+    base64url.fromBase64(data.publicKey.challenge)
+  );
+
+  for (let allowCred of data.publicKey.allowCredentials) {
+    allowCred.id = base64url.decode(base64url.fromBase64(allowCred.id));
+  }
+
+  navigator.credentials.get(data).then((response) => {
+    const data = publicKeyCredentialToJSON(response);
+    sendLoginResponse(data);
+  });
+};
+
+let seconds = 0;
+
+const sendLoginResponse = (data) => {
+  fetch('https://r02921049.ddns.net:10443/v0/login/finish', {
+    method: 'POST',
+    credentials: 'include',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    mode: 'cors',
+    body: JSON.stringify(data),
+  })
+  .then((response) => {
+    if (response.status !== 200) {
+      throw Error(response.statusText);
+    }
+
+    const token = response.headers.get('Authorization');
+    if (!!token) {
+      localStorage.setItem('token', token);
+    } else {
+      throw Error('no response token');
+    }
+    
+    
+    return response.json()
+  })
+  .then((response) => {
+    if (response.email === $('#login_email').val()) {
+      $('#dateForm').show();
+
+      seconds = 0;
+      test();
+    } else {
+      throw Error('error');
+    }
+  })
+  .catch(console.error);
+};
+
+const test = () => {
+  fetch('https://r02921049.ddns.net:10443/v0/test', {
+    method: 'GET',
+    credentials: 'include',
+    headers: {
+      'Authorization': localStorage.getItem('token'),
+      'Content-Type': 'application/json'
+    },
+    mode: 'cors',
+  })
+  .then((response) => {
+    if (response.status !== 200) {
+      throw Error(response.statusText);
+    }
+
+    if ($('#remember').prop('checked')) {
+      const token = response.headers.get('Authorization');
+      if (!!token) {
+        localStorage.setItem('token', token);
+      } else {
+        throw Error('no response token');
+      }
+    }
+    return response.json();
+  })
+  .then((response) => {
+    seconds++;
+    $('#login_result').html(`<pre>${seconds}. ${JSON.stringify(response)}</pre>`);
+    setTimeout(test, 1000);
+  })
+  .catch((err) => {
+    $('#login_result').html(`<font colo="red"><pre>${JSON.stringify(err.message)}</pre></font>`);
+    $('#dateForm').hide();
+  });
+}
