227e7ca768dc6117d08ec3fdb4b0fb7ec442e00b
Yi-Hao Chiu <chokeslam46@gmail.com>
2019-06-02 23:43:32 +0800

    prettier & response handling

--- date.js
+++ date.js
@@ -1,11 +1,11 @@
 const padding = (val, len = 2) => {
   let padded = '';
   for (let index = 0; index < len; index++) {
-      padded += '0';
+    padded += '0';
   }
-  const result = `${padded}${val}`
+  const result = `${padded}${val}`;
   return result.slice(result.length - len);
-}
+};
 
 const dateToDays = () => {
   const date = new Date($('#mfc-date').val());
@@ -17,51 +17,54 @@ const dateToDays = () => {
 
   $('#mfc-days').val(padding(days.toString(16).toUpperCase(), 4));
   showResult();
-}
+};
 
 const daysToDate = () => {
   let days;
   try {
-      days = parseInt($('#mfc-days').val(), 16);
-      $('#mfc-days').val(padding(days.toString(16).toUpperCase(), 4));
+    days = parseInt($('#mfc-days').val(), 16);
+    $('#mfc-days').val(padding(days.toString(16).toUpperCase(), 4));
   } catch (error) {
-      console.error(error);
-      init();
-      return;
+    console.error(error);
+    init();
+    return;
   }
 
   const year = (days >> 9) + 1980;
-  const month = (days & 0x1F0) >> 5;
-  const dayOfMonth = days & 0x1F;
+  const month = (days & 0x1f0) >> 5;
+  const dayOfMonth = days & 0x1f;
 
   if (year === 0 || month === 0 || dayOfMonth === 0) {
-      console.error(`invalid date: ${year}-${padding(month)}-${padding(dayOfMonth)}`);
-      init();
-      return;
+    console.error(
+      `invalid date: ${year}-${padding(month)}-${padding(dayOfMonth)}`
+    );
+    init();
+    return;
   }
 
   $('#mfc-date').val(`${year}-${padding(month)}-${padding(dayOfMonth)}`);
 
   showResult();
-}
+};
 
 const showResult = () => {
   let days;
   let daysByteString;
   try {
-      days = parseInt($('#mfc-days').val(), 16);
-      daysByteString=`${padding((days & 0xFF).toString(16).toUpperCase())} ${padding((days >> 8).toString(16).toUpperCase())}`;
+    days = parseInt($('#mfc-days').val(), 16);
+    daysByteString = `${padding(
+      (days & 0xff).toString(16).toUpperCase()
+    )} ${padding((days >> 8).toString(16).toUpperCase())}`;
   } catch (error) {
-      console.error(error);
-      init();
-      return;
+    console.error(error);
+    init();
+    return;
   }
-  
+
   $('#result').html(`
       <div class="col-2">S7B0</div>
       <div class="col-10">4A ${daysByteString} 00 00 00 00 00 00 00 00 00 1E 00 00 00 00 00</div>
       <div class="col-2">S8B0</div>
       <div class="col-10">4A ${daysByteString} 00 00 00 00 00 1E 00 00 00 00 00 00 00 00 00</div>
   `);
-}
-
+};
--- fido2.js
+++ fido2.js
@@ -9,24 +9,26 @@ const register = () => {
     method: 'POST',
     credentials: 'include',
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
     },
     mode: 'cors',
     body: JSON.stringify({ email, displayname }),
   })
-  .then((response) => {
-    if (response.status !== 200) {
-      throw Error(response.statusText);
-    }
-    return response.json();
-  })
-  .then((response) => {
-    createCredential(response);
-  })
-  .catch((err) => {
-    $('#login_result').html(`<font colo="red"><pre>${JSON.stringify(err.message)}</pre></font>`);
-    $('#dateForm').hide();
-  });
+    .then((response) => {
+      if (response.status !== 200) {
+        throw Error(response.statusText || `${response.status} Error`);
+      }
+      return response.json();
+    })
+    .then((response) => {
+      createCredential(response);
+    })
+    .catch((err) => {
+      $('#register_result').html(
+        `<pre class="error">${JSON.stringify(err.message)}</pre>`
+      );
+      $('#dateForm').hide();
+    });
 };
 
 const createCredential = (data) => {
@@ -37,10 +39,18 @@ const createCredential = (data) => {
     base64url.fromBase64(data.publicKey.user.id)
   );
 
-  navigator.credentials.create(data).then((response) => {
-    const data = publicKeyCredentialToJSON(response);
-    sendRegisterResponse(data);
-  });
+  navigator.credentials
+    .create(data)
+    .then((response) => {
+      const data = publicKeyCredentialToJSON(response);
+      sendRegisterResponse(data);
+    })
+    .catch((err) => {
+      $('#register_result').html(
+        `<pre class="error">${JSON.stringify(err.message)}</pre>`
+      );
+      $('#dateForm').hide();
+    });
 };
 
 const publicKeyCredentialToJSON = (pubKeyCred) => {
@@ -65,7 +75,7 @@ const publicKeyCredentialToJSON = (pubKeyCred) => {
     return obj;
   }
 
-  return pubKeyCred || "";
+  return pubKeyCred || '';
 };
 
 const sendRegisterResponse = (data) => {
@@ -73,24 +83,26 @@ const sendRegisterResponse = (data) => {
     method: 'POST',
     credentials: 'include',
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
     },
     mode: 'cors',
     body: JSON.stringify(data),
   })
-  .then((response) => {
-    if (response.status !== 200) {
-      throw Error(response.statusText);
-    }
-    return response.json();
-  })
-  .then((response) => {
-    console.log(response);
-  })
-  .catch((err) => {
-    $('#login_result').html(`<font colo="red"><pre>${JSON.stringify(err.message)}</pre></font>`);
-    $('#dateForm').hide();
-  });
+    .then((response) => {
+      if (response.status !== 200) {
+        throw Error(response.statusText || `${response.status} Error`);
+      }
+      return response.json();
+    })
+    .then((response) => {
+      $('#register_result').html(`<pre>${JSON.stringify(response)}</pre>`);
+    })
+    .catch((err) => {
+      $('#register_result').html(
+        `<pre class="error">${JSON.stringify(err.message)}</pre>`
+      );
+      $('#dateForm').hide();
+    });
 };
 
 const login = () => {
@@ -103,24 +115,26 @@ const login = () => {
     method: 'POST',
     credentials: 'include',
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
     },
     mode: 'cors',
     body: JSON.stringify({ email }),
   })
-  .then((response) => {
-    if (response.status !== 200) {
-      throw Error(response.statusText);
-    }
-    return response.json();
-  })
-  .then((response) => {
-    assertCredential(response);
-  })
-  .catch((err) => {
-    $('#login_result').html(`<font colo="red"><pre>${JSON.stringify(err.message)}</pre></font>`);
-    $('#dateForm').hide();
-  });
+    .then((response) => {
+      if (response.status !== 200) {
+        throw Error(response.statusText || `${response.status} Error`);
+      }
+      return response.json();
+    })
+    .then((response) => {
+      assertCredential(response);
+    })
+    .catch((err) => {
+      $('#login_result').html(
+        `<pre class="error">${JSON.stringify(err.message)}</pre>`
+      );
+      $('#dateForm').hide();
+    });
 };
 
 const assertCredential = (data) => {
@@ -132,53 +146,64 @@ const assertCredential = (data) => {
     allowCred.id = base64url.decode(base64url.fromBase64(allowCred.id));
   }
 
-  navigator.credentials.get(data).then((response) => {
-    const data = publicKeyCredentialToJSON(response);
-    sendLoginResponse(data);
-  });
+  navigator.credentials
+    .get(data)
+    .then((response) => {
+      const data = publicKeyCredentialToJSON(response);
+      sendLoginResponse(data);
+    })
+    .catch((err) => {
+      $('#login_result').html(
+        `<pre class="error">${JSON.stringify(err.message)}</pre>`
+      );
+      $('#dateForm').hide();
+    });
 };
 
 let seconds = 0;
+let timeout;
 
 const sendLoginResponse = (data) => {
   fetch('https://e711.csie.pccu.edu.tw:9000/v0/login/finish', {
     method: 'POST',
     credentials: 'include',
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
     },
     mode: 'cors',
     body: JSON.stringify(data),
   })
-  .then((response) => {
-    if (response.status !== 200) {
-      throw Error(response.statusText);
-    }
+    .then((response) => {
+      if (response.status !== 200) {
+        throw Error(response.statusText || `${response.status} Error`);
+      }
 
-    const token = response.headers.get('Authorization');
-    if (!!token) {
-      localStorage.setItem('token', token);
-    } else {
-      throw Error('no response token');
-    }
-    
-    
-    return response.json()
-  })
-  .then((response) => {
-    if (response.email === $('#login_email').val()) {
-      $('#dateForm').show();
+      const token = response.headers.get('Authorization');
+      if (!!token) {
+        localStorage.setItem('token', token);
+      } else {
+        throw Error('no response token');
+      }
 
-      seconds = 0;
-      test();
-    } else {
-      throw Error('error');
-    }
-  })
-  .catch((err) => {
-    $('#login_result').html(`<font colo="red"><pre>${JSON.stringify(err.message)}</pre></font>`);
-    $('#dateForm').hide();
-  });
+      return response.json();
+    })
+    .then((response) => {
+      if (response.email === $('#login_email').val()) {
+        $('#dateForm').show();
+
+        seconds = 0;
+        clearTimeout(timeout);
+        test();
+      } else {
+        throw Error('error');
+      }
+    })
+    .catch((err) => {
+      $('#login_result').html(
+        `<pre class="error">${JSON.stringify(err.message)}</pre>`
+      );
+      $('#dateForm').hide();
+    });
 };
 
 const test = () => {
@@ -186,38 +211,40 @@ const test = () => {
     method: 'GET',
     credentials: 'include',
     headers: {
-      'Authorization': localStorage.getItem('token'),
-      'Content-Type': 'application/json'
+      Authorization: localStorage.getItem('token'),
+      'Content-Type': 'application/json',
     },
     mode: 'cors',
   })
-  .then((response) => {
-    if (response.status !== 200) {
-      throw Error(response.statusText);
-    }
-
-    if ($('#remember').prop('checked')) {
-      const token = response.headers.get('Authorization');
-      if (!!token) {
-        localStorage.setItem('token', token);
-      } else {
-        throw Error('no response token');
+    .then((response) => {
+      if (response.status !== 200) {
+        throw Error(response.statusText || `${response.status} Error`);
       }
-    }
-    return response.json();
-  })
-  .then((response) => {
-    seconds++;
-    $('#login_result').html(
-`<pre>
+
+      if ($('#remember').prop('checked')) {
+        const token = response.headers.get('Authorization');
+        if (!!token) {
+          localStorage.setItem('token', token);
+        } else {
+          throw Error('no response token');
+        }
+      }
+      return response.json();
+    })
+    .then((response) => {
+      seconds++;
+      $('#login_result').html(
+        `<pre>
 Hello, ${response.displayname}
 ${seconds}. ${JSON.stringify(response)}
 </pre>`
-  );
-    setTimeout(test, 1000);
-  })
-  .catch((err) => {
-    $('#login_result').html(`<font colo="red"><pre>${JSON.stringify(err.message)}</pre></font>`);
-    $('#dateForm').hide();
-  });
-}
+      );
+      timeout = setTimeout(test, 1000);
+    })
+    .catch((err) => {
+      $('#login_result').html(
+        `<pre class="error">${JSON.stringify(err.message)}</pre>`
+      );
+      $('#dateForm').hide();
+    });
+};
